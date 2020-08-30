a61e0fff86913802848a0dc2793162e1b78026e3
atkuzmanov <atkuzmanov@gmail.com>
2020-08-30 17:45:22 +0300

    Work-In-Progress-WIP-2020-08-30-174522

---
+++ java-head-first-playground/src/head/first/java/SimpleDotComGame/SimpleDotComGame.java
@@ -0,0 +1,9 @@
+package head.first.java.SimpleDotComGame;
+
+public class SimpleDotComGame {
+    public static void main(String[] args) {
+        SimpleDotCom dot1 = new SimpleDotCom();
+
+        
+    }
+}
---
+++ java-head-first-playground/src/head/first/java/SimpleDotComGame/SimpleDotComTest.java
@@ -0,0 +1,29 @@
+package head.first.java.SimpleDotComGame;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleDotComTest {
+
+    @Test
+    public void test1() {
+        SimpleDotCom dot1 = new SimpleDotCom();
+
+        int[] locations = {2, 3, 4};
+
+        dot1.setLocationCells(locations);
+
+        String userGuess = "2";
+
+        String result = dot1.checkYourself(userGuess);
+
+        String testResult = "failed";
+
+        if(result.equals("hit")) {
+            testResult = "passed";
+        }
+
+        System.out.println(">>> " + testResult);
+
+        assert (testResult.equals("passed"));
+    }
+}
