6863e0a9b2267322b8af5aed9249725e99b39480
atkuzmanov <atkuzmanov@gmail.com>
2020-11-15 23:56:24 +0200

    Work-In-Progress-WIP-2020-11-15-235624

--- java-minesweeper-example-1-2020-11-14/src/main/java/com/minesweeper/example/one/Minesweeper.java
+++ java-minesweeper-example-1-2020-11-14/src/main/java/com/minesweeper/example/one/Minesweeper.java
@@ -2,12 +2,19 @@ package com.minesweeper.example.one;
 
 import java.util.Random;
 
+/**
+ * References:
+ * https://www.geeksforgeeks.org/cpp-implementation-minesweeper-game/
+ */
 public class Minesweeper {
 
     private static final int ROWS = 7;
     private static final int COLS = 7;
     private static final int MINES = 5;
+    // Stores (x,y) coordinates of all mines.
     private static final int MINES_COORS[][] = new int[MINES][2];
+
+    private static int MOVES_LEFT = ROWS * COLS - MINES;
     private static char[][] realBoard = new char[ROWS][COLS];
     private static char[][] playerBoard = new char[ROWS][COLS];
 
@@ -70,7 +77,7 @@ public class Minesweeper {
 
             if (marked[random] == false) {
                 // Row Index of the Mine.
-                MINES_COORS[i][0]= x;
+                MINES_COORS[i][0] = x;
                 // Column Index of the Mine.
                 MINES_COORS[i][1] = y;
                 // Place the mine
@@ -79,17 +86,96 @@ public class Minesweeper {
                 i++;
             }
         }
+    }
+
+    /*
+        Count all the mines in the 8 adjacent
+        cells
+
+            N.W   N   N.E
+              \   |   /
+               \  |  /
+            W----Cell----E
+                 / | \
+               /   |  \
+            S.W    S   S.E
+
+        Cell-->Current Cell (row, col)
+        N -->  North        (row-1, col)
+        S -->  South        (row+1, col)
+        E -->  East         (row, col+1)
+        W -->  West            (row, col-1)
+        N.E--> North-East   (row-1, col+1)
+        N.W--> North-West   (row-1, col-1)
+        S.E--> South-East   (row+1, col+1)
+        S.W--> South-West   (row+1, col-1)
+     */
+    private static int countAdjacentMines(int row, int col, char realBoard[][]) {
+        int count = 0;
+
+        //----------- 1st Neighbour (North) ------------
+        // Only process this cell if this is a valid one
+        if (isValid(row - 1, col)) {
+            if (isMine(row - 1, col, realBoard)) {
+                count++;
+            }
+        }
+        //----------- 2nd Neighbour (South) ------------
+        // Only process this cell if this is a valid one
+        if (isValid(row + 1, col)) {
+            if (isMine(row + 1, col, realBoard))
+                count++;
+        }
+        //----------- 3rd Neighbour (East) ------------
+        // Only process this cell if this is a valid one
+        if (isValid(row, col + 1)) {
+            if (isMine(row, col + 1, realBoard))
+                count++;
+        }
+        //----------- 4th Neighbour (West) ------------
+        // Only process this cell if this is a valid one
+        if (isValid(row, col - 1)) {
+            if (isMine(row, col - 1, realBoard))
+                count++;
+        }
+        //----------- 5th Neighbour (North-East) ------------
+        // Only process this cell if this is a valid one
+        if (isValid(row - 1, col + 1)) {
+            if (isMine(row - 1, col + 1, realBoard))
+                count++;
+        }
+        //----------- 6th Neighbour (North-West) ------------
+        // Only process this cell if this is a valid one
+        if (isValid(row - 1, col - 1)) {
+            if (isMine(row - 1, col - 1, realBoard))
+                count++;
+        }
+        //----------- 7th Neighbour (South-East) ------------
+        // Only process this cell if this is a valid one
+        if (isValid(row + 1, col + 1)) {
+            if (isMine(row + 1, col + 1, realBoard))
+                count++;
+        }
+        //----------- 8th Neighbour (South-West) ------------
+        // Only process this cell if this is a valid one
+        if (isValid(row + 1, col - 1)) {
+            if (isMine(row + 1, col - 1, realBoard))
+                count++;
+        }
+
+        return count;
+    }
+
+    private static void makeAMove() {
 
     }
 
     public static void main(String[] args) {
+
         initialise();
         printBoard(playerBoard);
         placeRandomMines();
         printBoard(realBoard);
 
-//        for(int i = 0; i < 10; i++) {
-//            System.out.println(getRandNum(0, 7) % (ROWS * COLS));
-//        }
     }
 }
