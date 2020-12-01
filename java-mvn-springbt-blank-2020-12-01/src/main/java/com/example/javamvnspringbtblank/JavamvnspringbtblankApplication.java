9453ea29084a9d95b0768d5f9295391b1c23d1e4
atkuzmanov <atkuzmanov@gmail.com>
2020-12-01 18:49:35 +0200

    Work-In-Progress-WIP-2020-12-01-184935

---
+++ java-mvn-springbt-blank-2020-12-01/src/main/java/com/example/javamvnspringbtblank/JavamvnspringbtblankApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamvnspringbtblank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamvnspringbtblankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavamvnspringbtblankApplication.class, args);
+	}
+
+}
